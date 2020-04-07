dd37518237837c55f8b95250ce9dd77adc0cdf62
frottier <frottier@users.noreply.github.com>
2020-04-07 17:58:42 +0200

    create dummy list on first run

--- all-eye-did.sh
+++ all-eye-did.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+
 # set location of script as destination
+
 dest="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo "writing to"
 echo $dest
 echo 
+
+
+# create dummy output list on first run
+
+if [ ! -s "$dest"/history_as_we_know_it ]; then
+  touch "$dest"/history_as_we_know_it
+fi
+
+# append new stuff, sort and delete duplicates
 
 cp "$dest"/history_as_we_know_it "$dest"/tmp_old_history
 cat ~/.bash_history > "$dest"/tmp_recent_history
