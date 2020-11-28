f2b9fcbde087411f04000d43007fcdf0b8aa8518
frottier <handtuch@gmx.ch>
2020-11-28 19:44:27 +0100

    add option for arbitrary input file

--- all-eye-did.sh
+++ all-eye-did.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
 
+# parse command line argument as input file
+
+if [ -z "$1"  ]; then
+  input_file=~/.bash_history
+else
+  input_file="$1"
+fi
+
+if [ ! -f "$input_file" ]; then
+  echo "I doubt the existence of the file you gave me."
+  echo "Maybe try something else."
+  exit 0
+fi
+
+
 # set location of script as destination
 
 dest="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -19,7 +34,7 @@ fi
 # append new stuff, sort and delete duplicates
 
 cp "$dest"/history_as_we_know_it "$dest"/tmp_old_history
-cat ~/.bash_history > "$dest"/tmp_recent_history
+cat "$input_file" > "$dest"/tmp_recent_history
 
 sort -u "$dest"/tmp_old_history "$dest"/tmp_recent_history > "$dest"/history_as_we_know_it
 
